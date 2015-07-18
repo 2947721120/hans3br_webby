2546b1c8ea1a15b0301ea7a97535f7f7395de9f4
Jason Hansell <jasonhansell@gmail.com>
2015-07-18 00:39:48 +0000

    version 1.0 build

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,22 +17,70 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   //create the default coating pages to show
   app.pages = [
     {
-      pageName: "DrugLayering", 
+      pageName: "CoatingLayer1", 
       pageTitle: "Drug Layering",
       productTitle: "Opadry 03K",
     },
     {
-      pageName: "SealCoating", 
+      pageName: "CoatingLayer2", 
       pageTitle: "Seal Coating",
       productTitle: "Opadry YS-1",
     },
     {
-      pageName: "BarrierMembrane", 
+      pageName: "CoatingLayer3", 
       pageTitle: "Barrier Membrane",
       productTitle: "Surelease",
     }
   ];
-    
+  
+  app.SUGLETS = [
+    {label:'Suglet 16/18', diameter: 1100, min:1000, max:1180, density:1.5},
+    {label:'Suglet 18/20', diameter: 900, min:850, max:1000, density:1.5},
+    {label:'Suglet 20/25', diameter: 800, min:710, max:850, density: 1.5},
+    {label:'Suglet 25/30', diameter: 665, min:600, max:710, density: 1.5},
+    {label:'Suglet 30/35', diameter: 550, min:500, max:600, density:1.5},
+    {label:'Suglet 45/60', diameter: 300, min:250, max:355, density:1.5},
+    {label:'Suglet 60/80', diameter: 215, min:180, max:250, density: 1.5},
+    {label:'Custom Size', diameter: 750, min:180, max:1200, density:1.5}
+  ];  
+  
+  app.CAPSULES = [
+    {label:'Size 000', volume:'1.37'},
+    {label:'Size 00', volume:'0.95'},
+    {label:'Size 0', volume:'0.68'},
+    {label:'Size 1', volume:'0.50'},
+    {label:'Size 2', volume:'0.37'},
+    {label:'Size 3', volume:'0.30'},
+    {label:'Size 4', volume:'0.21'},
+    {label:'Size 5', volume:'0.13'},
+    {label:'Custom Capsule', volume:'0.60'}
+  ];
+  
+  app.IR_COATINGS = [
+    {label:'Opadry 03A', density:'1.01'},
+    {label:'Opadry 03K', density:'1.01'},
+    {label:'Opadry YS-1', density:'1.01'},
+    {label:'Opadry II 85F', density:'1.01'},
+    {label:'Custom Binder', density:'1.01'}
+  ];
+                        
+  app.ENTERIC_COATINGS = [
+    {label:'Acryleze', density:'1.64'},
+    {label:'Acryleze II', density:'1.64'},
+    {label:'Opadry Enteric', density:'1.21'},
+    {label:'Custom Enteric', density:'1.01'}
+ ];
+                         
+  app.BARRIER_MEMBRANE_COATINGS = [
+    {label:'Surelease', density:'1.01'},
+    {label:'Ethocel', density:'1.21'},
+    {label:'Custom Barrier Membrane', density:'1.01'}
+  ];
+  
+  app.APPLICATIONS = ["Drug Layering", "Seal Coating", "Enteric Coating", "Barrier Membrane", "Custom Coating"];
+  
+  
+  
   app.displayInstalledToast = function() {
     document.querySelector('#caching-complete').show();
   };
@@ -45,13 +93,28 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   });
 
   window.addEventListener('dd-open-suglet-dialog', function() {
-    // imports are loaded and elements have been registered
     document.querySelector('#sugletDialog').toggle();
   });
   
-  window.addEventListener('dd-open-coating-dialog', function() {
+  window.addEventListener('dd-open-coating-dialog', function(event) {
     // imports are loaded and elements have been registered
-    document.querySelector('#coatingDialog').toggle();
+    var application = event.detail.application;
+    if(application == 'Enteric Coating'){
+      app.coatings = app.ENTERIC_COATINGS;
+    } else if(application == 'Barrier Membrane'){
+      app.coatings = app.BARRIER_MEMBRANE_COATINGS;
+    } else {
+      app.coatings = app.IR_COATINGS;
+    }
+    var dialog = document.querySelector('#coatingDialog');
+    dialog.querySelector('iron-selector').selected = null;
+    dialog.toggle();
+  });
+  
+  window.addEventListener('dd-open-application-dialog', function() {
+    var dialog = document.querySelector('#applicationDialog');
+    dialog.querySelector('iron-selector').selected = null;
+    dialog.toggle();
   });
   
   window.addEventListener('dd-open-capsule-dialog', function() {
@@ -89,9 +152,38 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   };
   
-  //store a coating layer index for create new pages
-  app.coatingLayerNumber = 0;
+  app._sugletSelected = function(event) {
+    var model = event.model.item;
+    var core = document.querySelector('dd-core-dashboard');
+    core.set('sugletName', model.label);
+    core.set('maxDiameter', model.max);
+    core.set('minDiameter', model.min);
+    core.set('apparentDensity', model.density);
+    core.set('diameter', model.diameter);
+  };
   
+  app._capsuleSelected = function(event) {
+    var model = event.model.item;
+    var capsule = document.querySelector('dd-capsule-dashboard');
+    capsule.capsuleSize = model.label;
+    capsule.capsuleVolumeMax = model.volume;
+  };
+  
+  app._applicationSelected = function(event) {
+    var selector = 'dd-coating-dashboard[page-name = ' + app.currentPage + ']';
+    var page = document.querySelector(selector);
+    page.coatingApplication = event.model.item;
+  };
+  
+  app._coatingSelected = function(event) {
+    var selector = 'dd-coating-dashboard[page-name = ' + app.currentPage + ']';
+    var page = document.querySelector(selector);
+    page.coatingFamily = event.model.item.label;
+    page.filmDenstiy = event.model.item.density;
+  };
+  
+  //store a coating layer index for create new pages
+  app.coatingLayerNumber = app.pages.length;
   //function to add a page
   app._addPage = function() {
     //we store the layer index and always increment by one
@@ -106,5 +198,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     //use polymer array api to push and notify of changes            
     app.push('pages', temp);
   };
+  
+  
+  
 
 })(document);
--- app/scripts/text.js
+++ app/scripts/text.js
@@ -4,58 +4,10 @@
 
 (function(document) {
     'use strict';
-
-    var SUGLETS = [
-                    {label:'Suglet 16/18', diameter: 1100, min:1000, max:1180, density:1.5},
-                    {label:'Suglet 18/20', diameter: 900, min:850, max:1000, density:1.5},
-                    {label:'Suglet 20/25', diameter: 800, min:710, max:850, density: 1.5},
-                    {label:'Suglet 25/30', diameter: 665, min:600, max:710, density: 1.5},
-                    {label:'Suglet 30/35', diameter: 550, min:500, max:600, density:1.5},
-                    {label:'Suglet 45/60', diameter: 300, min:250, max:355, density:1.5},
-                    {label:'Suglet 60/80', diameter: 215, min:180, max:250, density: 1.5},
-                    {label:'Custom Size', diameter: 750, min:180, max:1200, density:1.5}
-                ];
                 
-    var CAPSULES = [
-                    {label:'Size 000', volume:'1.37'},
-                    {label:'Size 00', volume:'0.95'},
-                    {label:'Size 0', volume:'0.68'},
-                    {label:'Size 1', volume:'0.50'},
-                    {label:'Size 2', volume:'0.37'},
-                    {label:'Size 3', volume:'0.30'},
-                    {label:'Size 4', volume:'0.21'},
-                    {label:'Size 5', volume:'0.13'},
-                    {label:'Custom Size', volume:'0.60'}
-                    ];
+    
                     
-    var DRUG_COATINGS = [
-                          {label:'Opadry 03A', density:'1.01'},
-                          {label:'Opadry 03K', density:'1.01'},
-                          {label:'Opadry YS-1', density:'1.01'},
-                          {label:'Opadry II 85F', density:'1.01'},
-                          {label:'Custom Binder', density:'1.01'}
-                        ];
-                        
-  var SEAL_COATINGS = [
-                        {label:'Opadry 03A', density:'1.01'},
-                        {label:'Opadry 03K', density:'1.01'},
-                        {label:'Opadry YS-1', density:'1.01'},
-                        {label:'Opadry II 85F', density:'1.01'},
-                        {label:'Custom Coating', density:'1.01'}
-                      ];
-                      
-  var ENTERIC_COATINGS = [
-                            {label:'Acryleze', density:'1.64'},
-                            {label:'Acryleze II', density:'1.64'},
-                            {label:'Opadry Enteric', density:'1.21'},
-                            {label:'Custom Enteric', density:'1.01'}
-                         ];
-                         
-  var BARRIER_MEMBRANE_COATINGS = [
-                                    {label:'Surelease', density:'1.01'},
-                                    {label:'Ethocel', density:'1.21'},
-                                    {label:'Custom Barrier Membrane', density:'1.01'}
-                                  ];
+    
                                   
 
 });
